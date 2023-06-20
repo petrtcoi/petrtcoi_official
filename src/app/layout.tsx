29a0cb5cf753192fb3e94660ca9e000834ab5266
petrtcoi <45001736+petrtcoi@users.noreply.github.com>
2023-06-20 13:23:51 +0300

    add yandex metrika

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script"
 import "./globals.css"
 
 import { UseDarkMode } from "@features/dark_mode"
@@ -6,7 +7,7 @@ export const metadata = {
   title: "Petr Tcoi Official Website",
   description:
     "Welcome to Petr Tcoi Official Website. A little information about me and my programming skills.",
-  icon: "/favicon.ico",
+  icons: "/favicon.ico",
 }
 
 export default function RootLayout({
@@ -16,6 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <YandexMetrika />
       <UseDarkMode />
       <body
         className="
@@ -26,5 +28,28 @@ export default function RootLayout({
         {children}
       </body>
     </html>
+  )
+}
+
+function YandexMetrika() {
+  return (
+    <Script
+      id="yandex-metrika"
+      strategy="afterInteractive"
+    >
+      {`
+                    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                    m[i].l=1*new Date();
+                    for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+                 
+                    ym(91236839, "init", {
+                         clickmap:true,
+                         trackLinks:true,
+                         accurateTrackBounce:true
+                    });
+                `}
+    </Script>
   )
 }
