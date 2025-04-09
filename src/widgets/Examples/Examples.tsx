73b32e23b5491d842c25cff918f506d08c839272
petrtcoi <45001736+petrtcoi@users.noreply.github.com>
2025-04-10 01:34:55 +0300

    add website

--- src/widgets/Examples/Examples.tsx
+++ src/widgets/Examples/Examples.tsx
@@ -7,8 +7,8 @@ import { useEffect, useReducer, useState } from 'react'
 import { darkModeAtom } from '@features/dark_mode'
 
 import Link from 'next/link'
-import kreditGuideImage from './assets/kreditguide.jpg'
 import radiatorluxImage from './assets/radiatorlux.jpg'
+import ReadAndBurnImage from './assets/readandburn.jpg'
 import velarShopImage from './assets/velarshop.jpg'
 
 export default function Examples() {
@@ -70,12 +70,12 @@ export default function Examples() {
 
 			<div className='flex flex-col gap-2 justify-start max-w-[400px] m-auto'>
 				<Link
-					href='https://kreditguide.ru'
+					href='https://readandburn.com'
 					target='_blank'
 				>
 					<Image
-						src={kreditGuideImage}
-						alt='KreditGuide.ru screenshot'
+						src={ReadAndBurnImage}
+						alt='ReadAndBurn.com screenshot'
 						loading='eager'
 						width={450}
 						height={450}
@@ -83,12 +83,10 @@ export default function Examples() {
 						className={'rounded-xl shadow-none hover:shadow-xl hover:cursor-pointer'}
 					/>
 				</Link>
-				<h3 className='text-sm font-semibold'>KreditGuide.ru</h3>
-				<p className='text-sm font-light'>
-					Online directory of microcredit organizations and credit card banks. There are sections for news and articles.
-				</p>
+				<h3 className='text-sm font-semibold'>ReadAndBurn.com</h3>
+				<p className='text-sm font-light'>Minimalistic service for exchanging disposable messages.</p>
 				<div className='flex flex-row gap-5 items-center justify-start text-xs font-semibold'>
-					<div>Astro</div>
+					<div>NextJs</div>
 					<div>Tailwind</div>
 					<div>Supabase</div>
 				</div>
