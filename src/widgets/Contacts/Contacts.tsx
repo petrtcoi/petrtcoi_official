b90a8f424b27991b6817eecc147a389f976edf95
petrtcoi <45001736+petrtcoi@users.noreply.github.com>
2026-01-11 21:56:04 +0100

    change email

--- src/widgets/Contacts/Contacts.tsx
+++ src/widgets/Contacts/Contacts.tsx
@@ -1,54 +1,52 @@
-import Link from "next/link"
-import EmailIcon from "./components/EmailIcon"
-import LinkedinIcon from "./components/LinkedinIcon"
-import TelegramIcon from "./components/TelegramIcon"
-import GithubIcon from "./components/GithubIcon"
+import Link from 'next/link'
+import EmailIcon from './components/EmailIcon'
+import GithubIcon from './components/GithubIcon'
+import LinkedinIcon from './components/LinkedinIcon'
+import TelegramIcon from './components/TelegramIcon'
 
 export default function Contacts() {
-  return (
-    <div
-      id="contacts"
-      className="mt-7"
-    >
-      <div className="mb-2 text-slate-500 dark:text-slate-400 font-light dark:font-thin">
-        contact me
-      </div>
-      <div className=" flex flex-row gap-6 text-slate-700 dark:text-slate-100 font-light dark:font-thin">
-        <Link
-          href="http://t.me/thepetrtcoi"
-          target={"_blank"}
-          rel="noreferrer"
-          className="group"
-        >
-          <TelegramIcon />
-        </Link>
+	return (
+		<div
+			id='contacts'
+			className='mt-7'
+		>
+			<div className='mb-2 text-slate-500 dark:text-slate-400 font-light dark:font-thin'>contact me</div>
+			<div className=' flex flex-row gap-6 text-slate-700 dark:text-slate-100 font-light dark:font-thin'>
+				<Link
+					href='http://t.me/thepetrtcoi'
+					target={'_blank'}
+					rel='noreferrer'
+					className='group'
+				>
+					<TelegramIcon />
+				</Link>
 
-        <Link
-          href="https://www.linkedin.com/in/petrtcoi/"
-          target={"_blank"}
-          rel="noreferrer"
-          className="group"
-        >
-          <LinkedinIcon />
-        </Link>
+				<Link
+					href='https://www.linkedin.com/in/petrtcoi/'
+					target={'_blank'}
+					rel='noreferrer'
+					className='group'
+				>
+					<LinkedinIcon />
+				</Link>
 
-        <Link
-          href="https://github.com/petrtcoi"
-          target={"_blank"}
-          rel="noreferrer"
-          className="group"
-        >
-          <GithubIcon />
-        </Link>
+				<Link
+					href='https://github.com/petrtcoi'
+					target={'_blank'}
+					rel='noreferrer'
+					className='group'
+				>
+					<GithubIcon />
+				</Link>
 
-        <Link
-          href="mailto:petrtcoi@gmail.com"
-          target={"_blank"}
-          className="group"
-        >
-          <EmailIcon />
-        </Link>
-      </div>
-    </div>
-  )
+				<Link
+					href='mailto:petrtsoimail@gmail.com'
+					target={'_blank'}
+					className='group'
+				>
+					<EmailIcon />
+				</Link>
+			</div>
+		</div>
+	)
 }
